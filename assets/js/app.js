86257b90b3f5374f9262b2e25a4d5a38b402c7e8
Adria Hernandez <ahernandezlasc@gmail.com>
2024-09-26 12:09:49 -0600

    Se resuelve el ejercicio de revisión de código de otros

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,35 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+// Nombres de las variables se actualizan y se describen según clases
+const nameElem = document.querySelector('.name');
+const blogElem = document.querySelector('.blog');
+const locationElem = document.querySelector('.location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+// Se agregan parámetros a las funciones
+function displayUser(username,nameElem,blogElem,locationElem) {
+  nameElem.textContent = 'cargando...';
+
+  // Se remueve el await y se actualiza el nombre
+  const promise = fetch(`${usersEndpoint}/${username}`,{method: "GET"});
+
+  // Se establece el comportamiento de la promesa
+  promise
+  .then(response => {response.json()
+    .then(response => {
+      console.log(response);
+      nameElem.textContent = `${response.name}`;
+      blogElem.textContent = `${response.blog}`;
+      locationElem.textContent = `${response.location}`;
+      })
+      // Se captura el error en la promesa
+  }).catch((error) => {handleError(error)})
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  nameElem.textContent = `Algo salió mal: ${err}`
 }
 
-displayUser('stolinski').catch(handleError);
+// Se elimina la captura de error en la llamada a la función
+displayUser('stolinski',nameElem,blogElem,locationElem)
